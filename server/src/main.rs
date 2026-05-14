5c2be63403818143a3f8dd788cc12572678080c1
KubanDobby <ed_shamshurin@icloud.com>
2026-05-14 10:20:42 +0300

    Заготовки под ручки для создания и получения точек

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+use tower_http::cors::CorsLayer;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/points", post(handlers::create_point))
+        .route("/points", get(handlers::get_points))
+        .layer(CorsLayer::permissive());
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    println!("Server started on :3000");
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
