9a533bec4d095c3a26e0ff628fa3d2985432fe05
KubanDobby <ed_shamshurin@icloud.com>
2026-05-14 10:06:16 +0300

    fix

--- server/target/debug/deps/map_points_api-15fd6adbc8d1ed34.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Data/Projects/map-points/server/target/debug/deps/map_points_api-15fd6adbc8d1ed34.d: src/main.rs
-
-/Volumes/Data/Projects/map-points/server/target/debug/deps/map_points_api-15fd6adbc8d1ed34: src/main.rs
-
-src/main.rs:
--- server/target/debug/map-points-api.d
+++
@@ -1,1 +0,0 @@
-/Volumes/Data/Projects/map-points/server/target/debug/map-points-api: /Volumes/Data/Projects/map-points/server/src/main.rs
